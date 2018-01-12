7a3e27320c3a6c5bf2131e16e83aecf4cfb77e08
Ryan Rudolph <rudolphdesigns@gmail.com>
2018-01-12 15:03:39 -0500

    Add files via upload

--- contact-info-bar.php
+++ contact-info-bar.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              https://www.ryanrudolph.com
- * @since             1.0.0
+ * @since             0.0.1
  * @package           Contact_Info_Bar
  *
  * @wordpress-plugin
  * Plugin Name:       Contact Info Bar
  * Plugin URI:        https://getphound.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Description:       Add contact information to the top of your website.
+ * Version:           0.0.1
  * Author:            Ryan Rudolph
  * Author URI:        https://www.ryanrudolph.com
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'PLUGIN_NAME_VERSION', '0.0.1' );
 
 /**
  * The code that runs during plugin activation.
